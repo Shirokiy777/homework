36dff9117aaf870a3e0c1332ac3b600949e8a5f6
Shirokiy777 <79170807+Shirokiy777@users.noreply.github.com>
2021-02-23 13:13:07 +0300

    HomeWork1

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's ur name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name} ! Cегодня {DateTime.Now} ");
+        }
+    }
+}
