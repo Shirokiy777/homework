049eeed4260ff01f70736c38384fc4ccdf9a9e72
Shirokiy777 <79170807+Shirokiy777@users.noreply.github.com>
2021-03-04 11:49:56 +0300

    Homework 3-2

---
+++ Homework 3-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Homework_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Chars = "АБВГДЕЖЗИКЛМНОПРСТУХЦШЩЮЭЯ";
+            int i;
+            int j;
+            Random rnd = new Random();
+            long number;
+            long[,] array = new long[5, 2];
+            Console.WriteLine("   НОМЕР    ИНИЦИАЛЫ");
+            for (i = 0; i < array.GetLength(0); i++)
+            {
+                array[i, 0] = number = rnd.Next(000000000, 999999999);
+                Console.Write($"+79{array[i, 0]} ");
+                for (j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(Chars[rnd.Next(0, Chars.Length)] + ".");
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
