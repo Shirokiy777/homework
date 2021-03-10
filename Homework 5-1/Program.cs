944e5c00773da31666242ecc3cae4679ee7d6e70
Shirokiy777 <79170807+Shirokiy777@users.noreply.github.com>
2021-03-10 10:46:23 +0300

    Homework 5-1

---
+++ Homework 5-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Net.Mime;
+
+namespace Homework_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        string someText = Console.ReadLine();
+        File.WriteAllText("C:/Users/волк не волк/source/repos/lesson2/text.txt", someText);
+        }
+    }
+}
